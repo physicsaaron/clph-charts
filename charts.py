eec89a1bed7a0169fe1e58d018d73fe555c174e1
physicsaaron <104877336+physicsaaron@users.noreply.github.com>
2023-12-15 05:42:38 -0500

    Update charts.py

--- charts.py
+++ charts.py
@@ -3,17 +3,29 @@ import gspread
 from google.auth import default
 import os
 import shutil
+import time
 
 def prepare():
-  drive.mount('/content/drive')
+  try:
+    drive.mount('/content/drive')
+  except:
+    error=True
   auth.authenticate_user()
   creds, _ = default()
   gc = gspread.authorize(creds)
-  os.mkdir("/content/drive/My Drive/000_Nothing_Here_To_See")
+  try:
+    os.mkdir("/content/drive/My Drive/000_Nothing_Here_To_See")
+  except:
+    error=True
   sh = gc.create('O_o')
+  time.sleep(1)
   wb = gc.open('O_o').sheet1
   cell_list = wb.range('A1:Z1000')
   for cell in cell_list:
       cell.value="O_o"
   wb.update_cells(cell_list)
-  shutil.move("/content/drive/My Drive/O_o.gsheet", "/content/drive/My Drive/000_Nothing_Here_To_See")
+  time.sleep(2)
+  try:
+    shutil.move("/content/drive/My Drive/O_o.gsheet", "/content/drive/My Drive/000_Nothing_Here_To_See")
+  except:
+    os.remove("/content/drive/My Drive/O_o.gsheet")

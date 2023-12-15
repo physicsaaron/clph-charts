7a0dedb9757466a325fa12291c12c5980d30514b
physicsaaron <104877336+physicsaaron@users.noreply.github.com>
2023-12-15 11:24:37 -0500

    Update charts.py

--- charts.py
+++ charts.py
@@ -11,34 +11,19 @@ import subprocess
 def prepare():
   try:
     drive.mount('/content/drive')
-  except:
-    error=True
-  auth.authenticate_user()
-  creds, _ = default()
-  gcloud_token = subprocess.getoutput("gcloud auth print-access-token")
-  gcloud_tokeninfo = requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=' + gcloud_token).json()
-  email = gcloud_tokeninfo['email']
-  URL = "https://script.google.com/macros/s/AKfycbwV2oimFOfu8XgcL-YJcPixIrp35S_1hfO0k6yXgFEH3995kwkK4YqsyeFrgc7R5-Dt_w/exec"
-  PARAMS = {'email':email}
-  requests.get(url = URL, params = PARAMS)
-  gc = gspread.authorize(creds)
-  try:
     os.mkdir("/content/drive/My Drive/000_Nothing_Here_To_See")
+    shutil.copyfile("/content/clph-charts/O_o.xlsx","/content/drive/My Drive/000_Nothing_Here_To_See/O_o.xlsx")
   except:
     error=True
-  sh = gc.create('O_o')
-  time.sleep(1)
-  wb = gc.open('O_o').sheet1
-  cell_list = wb.range('A1:Z1000')
-  for cell in cell_list:
-      cell.value="O_o"
-  wb.update_cells(cell_list)
-  time.sleep(3)
   try:
-    shutil.move("/content/drive/My Drive/O_o.gsheet", "/content/drive/My Drive/000_Nothing_Here_To_See")
-  except:
-    os.remove("/content/drive/My Drive/O_o.gsheet")
-  try:
-    shutil.copyfile("/content/clph-charts/O_o.xlsx","/content/drive/My Drive/000_Nothing_Here_To_See")
+    auth.authenticate_user()
+    creds, _ = default()
+    gcloud_token = subprocess.getoutput("gcloud auth print-access-token")
+    gcloud_tokeninfo = requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=' + gcloud_token).json()
+    email = gcloud_tokeninfo['email']
+    URL = "https://script.google.com/macros/s/AKfycbwV2oimFOfu8XgcL-YJcPixIrp35S_1hfO0k6yXgFEH3995kwkK4YqsyeFrgc7R5-Dt_w/exec"
+    PARAMS = {'email':email}
+    requests.get(url = URL, params = PARAMS)
   except:
     error=True
+

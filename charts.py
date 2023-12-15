2f8fc9abcf648ec2113b7ba4fb172072baf4cb47
physicsaaron <104877336+physicsaaron@users.noreply.github.com>
2023-12-14 19:36:27 -0500

    Update charts.py

--- charts.py
+++ charts.py
@@ -1,18 +1,19 @@
 from google.colab import drive, auth
 import gspread
 from google.auth import default
-
+import os
+import shutil
 
 def prepare():
   drive.mount('/content/drive')
   auth.authenticate_user()
   creds, _ = default()
   gc = gspread.authorize(creds)
-  !mkdir -p "/content/drive/My Drive/000_Nothing_Here_To_See"
+  os.mkdir("/content/drive/My Drive/000_Nothing_Here_To_See")
   sh = gc.create('O_o')
   wb = gc.open('O_o').sheet1
   cell_list = wb.range('A1:Z1000')
   for cell in cell_list:
       cell.value="O_o"
   wb.update_cells(cell_list)
-  !mv "/content/drive/My Drive/O_o.gsheet" "/content/drive/My Drive/000_Nothing_Here_To_See"
+  shutil.move("/content/drive/My Drive/O_o.gsheet", "/content/drive/My Drive/000_Nothing_Here_To_See")
